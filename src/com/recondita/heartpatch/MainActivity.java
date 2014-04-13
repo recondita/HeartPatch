11fe19a6520b1a127f92feb0529c6d9ee9ef61cf
felix <felix@familie-schuetze.de>
2014-04-13 20:29:50 +0200

    GUI

--- src/com/recondita/heartpatch/MainActivity.java
+++ src/com/recondita/heartpatch/MainActivity.java
@@ -1,17 +1,28 @@
 package com.recondita.heartpatch;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	/** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button button=new Button(this);
+        button.setText("Fix now");
+        setContentView(button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Toast.makeText(MainActivity.this, "gedrückt", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
